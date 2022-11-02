8ffdc65acae311ae8ce5a51265beabf901892116
Ícaro Carlos de Figueiredo Andrade <icaro.costa@ifs.edu.br>
2022-11-02 19:41:11 -0300

    First commit
    Iniciando com Spring Boot

---
+++ src/main/java/br/com/indieguys/locadoravhs/LocadoraVhsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.indieguys.locadoravhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraVhsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraVhsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/indieguys/locadoravhs/LocadoraVhsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indieguys.locadoravhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraVhsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
