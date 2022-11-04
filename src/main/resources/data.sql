f0ea88ddd1eb2213841af623b328608cbd0b4ef5
Ícaro Carlos de Figueiredo Andrade <icaro.costa@ifs.edu.br>
2022-11-04 18:29:59 -0300

    Inserts na tabela de classificação indicativa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,45 @@
--- Lista dos gêneros retirada de https://pt.wikipedia.org/wiki/Lista_de_g%C3%AAneros_cinematogr%C3%A1ficos
+-- Definição da Classificação Indicativa encontrada em https://pt.wikipedia.org/wiki/Lista_de_g%C3%AAneros_cinematogr%C3%A1ficos
+
+INSERT INTO public.classificacao_indicativa(faixa_etaria_recomendada, descricao, violencia, sexo_nudez, drogas)
+	VALUES ('L', 'LIVRE',
+		'Arma sem violência; Morte sem violência; Ossada ou esqueleto sem violência; Violência Fantasiosa.',
+		'Nudez não erótica.',
+		'Consumo moderado ou insinuado de droga lícita.');
+
+INSERT INTO public.classificacao_indicativa(faixa_etaria_recomendada, descricao, violencia, sexo_nudez, drogas)
+	VALUES ('10', 'Não recomendado para menores de 10 (dez) anos',
+		'Angústia; Arma com violência; Ato criminoso sem violência; Linguagem depreciativa; Medo/tensão; Ossada ou esqueleto com resquício de ato de violência.',
+		'Conteúdo educativo sobre sexo.',
+		'Descrição do consumo de droga lícita; Discussão sobre o tema drogas; Uso medicinal de droga ilícita.');
+
+
+INSERT INTO public.classificacao_indicativa(faixa_etaria_recomendada, descricao, violencia, sexo_nudez, drogas)
+	VALUES ('12', 'Não recomendado para menores de 12 (doze) anos',
+		'Agressão verbal; Assédio sexual; Ato violento; Ato violento contra animal; Bullying; Descrição de violência; Exposição ao perigo; Exposição de cadáver; Exposição de pessoa em situação constrangedora ou degradante; Lesão corporal; Morte derivada de ato heróico; Morte natural ou acidental com dor ou violência; Obscenidade; Presença de sangue; Sofrimento da vítima; Supervalorização da beleza física; Supervalorização do consumo; Violência psicológica.',
+		'Apelo sexual; Carícia sexual; Insinuação sexual; Linguagem chula; Linguagem de conteúdo sexual; Masturbação; Nudez velada; Simulação de sexo.',
+		'Consumo de droga lícita; Consumo irregular de medicamento; Discussão sobre legalização de droga ilícita; Indução ao uso de droga lícita; Menção a droga ilícita.');
+
+
+INSERT INTO public.classificacao_indicativa(faixa_etaria_recomendada, descricao, violencia, sexo_nudez, drogas)
+	VALUES ('14', 'Não recomendado para menores de 14 (quatorze) anos',
+		'Aborto; Estigma/preconceito; Eutanásia; Exploração sexual; Morte intencional; Pena de morte.',
+		'Erotização; Nudez; Prostituição; Relação sexual; Vulgaridade.',
+		'Consumo insinuado de droga ilícita; Descrição do consumo ou tráfico de droga ilícita.');
+
+INSERT INTO public.classificacao_indicativa(faixa_etaria_recomendada, descricao, violencia, sexo_nudez, drogas)
+	VALUES ('16', 'Não recomendado para menores de 16 (dezesseis) anos',
+		'Ato de pedofilia; Crime de ódio; Estupro/coação sexual; Mutilação; Suicídio; Tortura; Violência gratuita/banalização da violência.',
+		'Relação sexual intensa.',
+		'Consumo de droga ilícita; Indução ao consumo de droga ilícita; Produção ou tráfico de droga ilícita.');
+
+INSERT INTO public.classificacao_indicativa(faixa_etaria_recomendada, descricao, violencia, sexo_nudez, drogas)
+	VALUES ('18', 'Não recomendado para menores de 18 (dezoito) anos',
+		'Apologia à violência; Crueldade.',
+		'Sexo explícito; Situação sexual complexa/de forte impacto.',
+		'Apologia ao uso de droga ilícita.');
+
+
+-- Definição dos Gêneros encontrada em https://pt.wikipedia.org/wiki/Lista_de_g%C3%AAneros_cinematogr%C3%A1ficos
 
 INSERT INTO public.genero(descricao) VALUES ('Ação');
 INSERT INTO public.genero(descricao) VALUES ('Aventura');
